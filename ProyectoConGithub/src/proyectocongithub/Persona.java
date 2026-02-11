986c418b709641887694bbdbfb9d02a442e85f5f
Diego Rodriguez <rodriguezdiego263@gmail.com>
2026-02-11 07:55:25 -0500

    Inicializando el proyecto

---
+++ ProyectoConGithub/src/proyectocongithub/Persona.java
@@ -0,0 +1,19 @@
+
+package proyectocongithub;
+
+public class Persona {
+    private int num;
+
+    public Persona(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+    
+}
